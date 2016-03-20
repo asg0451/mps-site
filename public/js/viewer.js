0c71d7101097e83b17da30a2e35cc6b9a2074c50
Miles Frankel <miles.frankel@gmail.com>
2016-03-19 20:09:42 -0400

    better map display

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -30,6 +30,21 @@ var refresh = function(mapData) {
     var name = mapData.name;
     var floors = mapData.floors;
 
+    if(floors.length > 3) {
+        $('.map-col').removeClass('col-md-4');
+        $('.map-col').addClass('col-md-3');
+        $('#r6-row-0').append(
+            $('<div>', {class: 'col-md-3 map-col map-col-optional'}).append(
+                $('<h3>', {id: 'r6-floor-name-3', class: 'r6-floor-name'}),
+                $('<img>', {id: 'r6-map-3', class: 'img-responsive r6-map-img'})
+            ));
+    }
+    else {
+        $('.map-col').removeClass('col-md-3');
+        $('.map-col').addClass('col-md-4');
+        $('.map-col-optional').remove();
+    }
+
     floors.forEach(function(floor,i) {
         $('#r6-map-'+i).attr('src', '/maps/' + name + '/' + floor);
         $('#r6-floor-name-'+i).html(floor);
